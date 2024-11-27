cb41453f182c41c2ed085f3792264c6faf8e92ff
KhaNH <nguyen_kha@kozo-japan.com>
2024-11-27 16:22:45 +0700

    feat:Create UI base

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          First app demo with React JS 
         </p>
         <a
           className="App-link"
